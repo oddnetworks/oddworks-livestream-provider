5442b925b207b3554fe5188c1edcdcf16011d4b6
Kris Walker <kris@kixx.name>
2017-11-15 09:00:15 -0500

    First pass at version 3.0.0
     On branch v3
     Changes to be committed:
    	new file:   cli.js
    	modified:   index.js
    	new file:   lib/api-client.js
    	new file:   lib/event-to-live-video-transform.js
    	new file:   lib/http-client.js
    	new file:   lib/library.js
    	new file:   lib/vod-to-video-transform.js
    	new file:   package-lock.json
    	modified:   package.json

---
+++ cli.js
@@ -0,0 +1,138 @@
+/* eslint-disable no-console */
+const yargs = require(`yargs`);
+const Provider = require(`./index`);
+
+const ENVIRONMENT_VARIABLES = [
+	`LIVESTREAM_SECRET_KEY`,
+	`LIVESTREAM_ACCOUNT_ID`
+];
+
+const REQUEST_METHODS = Object.freeze({
+	getAccounts: `{}`,
+	getVod: `{"eventId": "STRING", "videoId": "STRING"}`,
+	getLiveVideo: `{"eventId": "STRING"}`,
+	getAsset: `{THE CURSOR OBJECT}`
+});
+
+function parseCommandLineArguments() {
+	return yargs
+		.usage(`Usage: $0 <command> [options]`)
+		.command(`req`, `Make an API client request`, {
+			method: {
+				alias: `m`,
+				type: `string`,
+				demand: true,
+				describe: `Use the "list" command to see available methods`
+			},
+			args: {
+				alias: `a`,
+				default: `{}`,
+				describe: `Arguments object as a JSON string`
+			},
+			secretKey: {
+				describe: `Defaults to env var LIVESTREAM_SECRET_KEY`,
+				type: `string`
+			},
+			accountId: {
+				describe: `Defaults to env var LIVESTREAM_ACCOUNT_ID`,
+				type: `string`
+			}
+		})
+		.command(`list`, `List client methods`)
+		.help()
+		.argv;
+}
+
+function listCommand() {
+	console.log(`Request methods:`);
+	console.log(``);
+
+	console.log(`\tgetAccounts --args '{}'`);
+
+	Object.getOwnPropertyNames(Provider.prototype).forEach(key => {
+		if (REQUEST_METHODS[key] && key !== `constructor`) {
+			console.log(`\t${key} --args '${REQUEST_METHODS[key]}'`);
+		}
+	});
+
+	console.log(`\nEnvironment Variables:\n`);
+	ENVIRONMENT_VARIABLES.forEach(key => {
+		console.log(`\t`, key);
+	});
+
+	return Promise.resolve(null);
+}
+
+function requestCommand(args) {
+	const secretKey = args.secretKey;
+	const accountId = args.accountId;
+	const method = args.method;
+
+	if (!method) {
+		console.error(`An method is required (--method)`);
+		return Promise.resolve(null);
+	}
+
+	if (!secretKey) {
+		console.error(`An secretKey is required (--secretKey)`);
+		return Promise.resolve(null);
+	}
+
+	let params;
+	try {
+		params = JSON.parse(args.args);
+	} catch (err) {
+		console.error(`--args JSON parsing error:`);
+		console.error(err.message);
+		return Promise.resolve(null);
+	}
+
+	if (method === `getAccounts`) {
+		return Provider.getAccounts({secretKey}).then(res => {
+			console.log(JSON.stringify(res, null, 2));
+			return null;
+		});
+	}
+
+	if (!accountId) {
+		console.error(`An accountId is required (--accountId)`);
+		return Promise.resolve(null);
+	}
+
+	const provider = Provider.create({
+		accountId,
+		secretKey
+	});
+
+	if (typeof provider[method] !== `function`) {
+		console.error(`Invalid method "${method}": Provider#${method} is not a function.`);
+		return Promise.resolve(null);
+	}
+
+	return provider[method](params).then(res => {
+		console.log(JSON.stringify(res, null, 2));
+		return null;
+	});
+}
+
+exports.main = function () {
+	const argv = parseCommandLineArguments();
+	const command = argv._[0];
+
+	switch (command) {
+		case `list`:
+			return listCommand();
+		case `req`:
+			return requestCommand({
+				accountId: argv.accountId || process.env.LIVESTREAM_ACCOUNT_ID,
+				secretKey: argv.secretKey || process.env.LIVESTREAM_SECRET_KEY,
+				method: argv.method,
+				args: argv.args
+			});
+		default:
+			console.error(`A command argument is required.`);
+			console.error(`Use the --help flag to print out help.`);
+			return Promise.resolve(null);
+	}
+};
+
--- index.js
+++ index.js
@@ -1,1 +1,130 @@
 'use strict';
+
+const APIClient = require(`./lib/api-client`);
+const vodToVideoTransform = require(`./lib/vod-to-video-transform`);
+const eventToLiveVideoTransform = require(`./lib/event-to-live-video-transform`);
+
+class Provider {
+	constructor(spec) {
+		this.accountId = spec.accountId;
+
+		this.client = APIClient.createClient({
+			secretKey: spec.secretKey,
+			requestInterval: spec.requestInterval
+		});
+
+		// Make sure methods keep the correct `this` throughout chained async calls.
+		this.getVod = this.getVod.bind(this);
+		this.getLiveVideo = this.getLiveVideo.bind(this);
+	}
+
+	getVod(args) {
+		args = args || {};
+
+		const accountId = this.accountId;
+		const eventId = args.eventId;
+		const videoId = args.videoId;
+
+		if (!eventId || typeof eventId !== `string`) {
+			throw new Error(
+				`Missing required "eventId" parameter in Livestream Provider#getVod()`
+			);
+		}
+		if (!videoId || typeof videoId !== `string`) {
+			throw new Error(
+				`Missing required "videoId" parameter in Livestream Provider#getVod()`
+			);
+		}
+
+		return this.client(`/accounts/${accountId}/events/${eventId}/videos/${videoId}`);
+	}
+
+	getLiveVideo(args) {
+		args = args || {};
+
+		const accountId = this.accountId;
+		const eventId = args.eventId;
+
+		if (!eventId || typeof eventId !== `string`) {
+			throw new Error(
+				`Missing required "eventId" parameter in Livestream Provider#getLiveVideo()`
+			);
+		}
+
+		return this.client(`/accounts/${accountId}/events/${eventId}`);
+	}
+
+	getAsset(args) {
+		args = args || {};
+		const subProvider = args.subProvider;
+		args = args.args;
+
+		switch (subProvider) {
+			case `vod`:
+				return this.getVod(args).then(res => {
+					const video = vodToVideoTransform(res);
+					return {
+						data: video,
+						provider: `livestream`,
+						subProvider: `vod`,
+						args: {eventId: res.eventId, videoId: res.id},
+						source: res
+					};
+				});
+			case `live-video`:
+				return this.getLiveVideo(args).then(res => {
+					const video = eventToLiveVideoTransform(res);
+					return {
+						data: video,
+						provider: `livestream`,
+						subProvider: `live-video`,
+						args: {eventId: res.id},
+						source: res
+					};
+				});
+			default:
+				throw new Error(`No Livestream Provider method for subProvider "${subProvider}"`);
+		}
+	}
+
+	// Returns an Array of Livestream accounts associated with the given secretKey.
+	static getAccounts(args) {
+		args = args || {};
+		const secretKey = args.secretKey;
+
+		if (!secretKey || typeof secretKey !== `string`) {
+			throw new Error(
+				`Missing required "secretKey" parameter in Livestream Provider.getAccounts()`
+			);
+		}
+
+		const client = APIClient.createClient({secretKey});
+		return client(`/accounts`);
+	}
+
+	static create(options) {
+		options = options || {};
+		const accountId = options.accountId;
+		const secretKey = options.secretKey;
+		const requestInterval = options.requestInterval;
+
+		if (!accountId || typeof accountId !== `string`) {
+			throw new Error(
+				`Missing required "accountId" parameter in Livestream Provider.create()`
+			);
+		}
+		if (!secretKey || typeof secretKey !== `string`) {
+			throw new Error(
+				`Missing required "secretKey" parameter in Livestream Provider.create()`
+			);
+		}
+
+		return new Provider({
+			accountId,
+			secretKey,
+			requestInterval
+		});
+	}
+}
+
+module.exports = Provider;
